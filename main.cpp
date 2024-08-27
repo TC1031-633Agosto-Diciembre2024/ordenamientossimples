d52607b09fed873f8d37352f26774fffe51d802b
Andrés Torres <andxtorres22@gmail.com>
2024-08-27 10:20:43 -0600

    Insertion Sort

--- main.cpp
+++ main.cpp
@@ -20,15 +20,37 @@ void bubbleSort(int* arr,int size) {
                 swap(arr+j,arr+j+1);
             }
         }
-        std::cout<<"Iteración "<<i<<std::endl;
-        printArray(arr,size);
+    }
+}
+
+void selectionSort(int* arr, int size) {
+    for(int i=0;i<size;i++) {
+        int minIndex=i;
+        for(int j=i+1;j<size;j++) {
+            if(*(arr+j)<*(arr+minIndex)) {
+                minIndex=j;
+            }
+        }
+        swap(arr+i,arr+minIndex);
+    }
+}
+
+void insertionSort(int*arr, int size) {
+    for(int i=1;i<size;i++) {
+        int key=*(arr+i);
+        int j=i-1;
+        while(j>=0 && *(arr+j)>key) {
+            *(arr+j+1)=*(arr+j);
+            j--;
+        }
+        *(arr+j+1)=key;
     }
 }
 
 int main() {
-    int arr[]= {1,2,3,5,9,10};
+    int arr[]= {10,1,2,9,3,5};
     printArray(arr,6);
-    bubbleSort(arr,6);
+    selectionSort(arr,6);
     printArray(arr,6);
     return 0;
 }
